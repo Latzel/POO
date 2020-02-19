72ee99944748da03d6de210d83898888794fe1a6
Latzel <axel.dlrlopez@gmail.com>
2020-02-18 17:55:49 -0800

    peliculas con titulo y año

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -9,12 +9,22 @@ namespace Peliculas
         public string pais;
         public string director;
 
+
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            peliculas p1= new peliculas();
+            p1.titulo=("Titanic ");
+            p1.año=1997;
+            peliculas p2= new peliculas();
+            p2.titulo=("Forrest Gump ");
+            p2.año=1994;
+            Console.Write(p1.titulo);
+            Console.WriteLine(p1.año);
+            Console.Write(p2.titulo);
+            Console.WriteLine(p2.año);
         }
     }
 }
