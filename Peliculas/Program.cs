d1bf7036d9377c6cee6f8f3fa3e6189a2cd251fc
Latzel <axel.dlrlopez@gmail.com>
2020-02-18 17:36:04 -0800

    Atributos peliculas

---
+++ Peliculas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Peliculas
+{
+    class peliculas
+    {
+        public string titulo;
+        public int año;
+        public string pais;
+        public string director;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
