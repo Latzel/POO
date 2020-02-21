57eef70041e56f55ac222d299c51af8385dd475c
Latzel <axel.dlrlopez@gmail.com>
2020-02-20 17:35:03 -0800

    punto 4, private y metodos

--- Peliculas/Program.cs
+++ Peliculas/Program.cs
@@ -4,27 +4,65 @@ namespace Peliculas
 {
     class peliculas
     {
-        public string titulo;
-        public int año;
-        public string pais;
-        public string director;
+        private string titulo;
+        private int año;
+        private string pais;
+        private string director;
+
+        public string getTitulo(){
+            return titulo;
+        }
+
+        public void setTitulo(string t){
+            titulo=t;
+        }
+
+        public int getAño(){
+            return año;
+        }
+
+        public void setAño(int a){
+            año=a;
+        }
+
+        public string getPais(){
+            return pais;
+        }
+
+        public void setPais(string p){
+            pais=p;
+        }
+
+        public string getDirector(){
+            return director;
+        }
+        public void setDirector(string d){
+            director=d;
+        }
 
 
     }
+
     class Program
     {
         static void Main(string[] args)
         {
             peliculas p1= new peliculas();
-            p1.titulo=("Titanic ");
-            p1.año=1997;
+            p1.setTitulo("Titanic");
+            p1.setAño(1997);
+            p1.setPais("Estados Unidos");
+            p1.setDirector("James Cameron");
+            Console.WriteLine("{0}({1}) Pais: {2} Dirgida Por {3}", p1.getTitulo(), p1.getAño(), p1.getPais(), p1.getDirector());
+
             peliculas p2= new peliculas();
-            p2.titulo=("Forrest Gump ");
-            p2.año=1994;
-            Console.Write(p1.titulo);
-            Console.WriteLine(p1.año);
-            Console.Write(p2.titulo);
-            Console.WriteLine(p2.año);
+            p2.setTitulo("Forrest Gump");
+            p2.setAño(1994);
+            p2.setPais("Estados Unidos");
+            p2.setDirector("Robert Zemeckis");
+            Console.WriteLine("{0}({1}) Pais: {2} Dirigida Por {3}", p2.getTitulo(), p2.getAño(), p2.getPais(), p2.getDirector());
+            
+            
+            
         }
     }
 }
