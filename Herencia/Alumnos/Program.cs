b2a6d422283695c6b83c6eaa07c8c3e3150e361a
Latzel <axel.dlrlopez@gmail.com>
2020-04-02 15:45:27 -0700

    Herencia /Alumnos y Musicos

---
+++ Herencia/Alumnos/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Alumnos
+{
+    class Alumnos
+    {
+        protected string nombre;
+        protected string escuela;
+
+        public Alumnos(string n, string e){
+            nombre=n;
+            escuela=e;
+        }
+
+        public void presentarse(){
+            Console.WriteLine("Buenos dias, mi nombre es {0} y estudio en {1}", nombre, escuela);
+        }
+
+        public void explica(){
+            Console.WriteLine("Yo hago (Tal actividad)");
+        }
+    }
+
+    class Licenciatura:Alumnos
+    {
+        protected string carrera;
+
+        public Licenciatura(string nombre, string escuela, string c):base(nombre, escuela){
+         carrera=c;
+         }
+
+        public new void presentarse(){
+         Console.WriteLine("Soy {0} Mi licenciatura es en {1}",nombre, carrera);
+        }
+
+        public new void explica(){
+         Console.WriteLine("Por el momento hago Servicio social y residencia");
+         }
+    }
+
+    class Posgrado:Alumnos
+    {
+        protected string tema;
+
+        public Posgrado(string nombre, string escuela, string t):base(nombre, escuela){
+            tema=t;
+        }
+
+        public new void presentarse(){
+            Console.WriteLine("Buenos dias soy {0} vengo al posgrado", nombre);
+        }
+
+        public new void explica(){
+            Console.WriteLine("Mi tema de investigacion es {0}",tema);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumnos a = new Alumnos ("Juan", "Oxford");
+            Licenciatura l = new Licenciatura ("Travis","Mit","Sistemas");
+            Posgrado p = new Posgrado ("Kevin","Harvard","Pirateria Informatica");
+
+            a.presentarse();
+            a.explica();
+
+            l.presentarse();
+            l.explica();
+
+            p.presentarse();
+            p.explica();
+        }   
+    }
+}
---
+++ Herencia/Musicos/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Musicos
+{
+    class Musicos
+    {
+        protected string nombre;
+
+        public Musicos(string n){
+            nombre=n;
+        }
+
+        public void afina(){
+            Console.WriteLine("Denme un segundo");
+
+        }
+
+        public void saluda(){
+            Console.WriteLine("Hola a todos! Aqui {0}", nombre);
+        }
+
+        public void toca(){
+            Console.WriteLine("Vamos a darle!");
+        }
+
+    }
+
+    class Baterista:Musicos
+    {
+        protected string bateria;
+
+        public Baterista(string nombre, string bateria):base(nombre){
+            this.bateria=bateria;
+        }
+
+        public new void afina(){
+            Console.WriteLine("Batumps!");
+
+        }
+
+        public new void saluda(){
+            Console.WriteLine("Aqui {0} mi bateria es {1}",nombre, bateria);
+        }
+
+        public new void toca(){
+            Console.WriteLine("Pum! Pum! Pum!");
+        }
+    }
+
+    class Bajista:Musicos{
+         protected string bajo;
+
+        public Bajista(string nombre, string b):base(nombre){
+            bajo=b;
+        }
+        
+        public new void afina(){
+            Console.WriteLine("Tum...");
+
+        }
+
+        public new void saluda(){
+            Console.WriteLine("Aqui {0} mi bajo es un {1}",nombre, bajo);
+        }
+
+        public new void toca(){
+            Console.WriteLine("Tan tan");
+        }
+    }
+
+    class Guitarrista:Musicos{
+         protected string guitarra;
+
+        public Guitarrista(string nombre, string g):base(nombre){
+            guitarra=g;
+        }
+
+        public new void afina(){
+            Console.WriteLine("Pam Pam Pam");
+
+        }
+
+        public new void saluda(){
+            Console.WriteLine("Yo soy {0} mi guitarra es {1} y nosotros somos Hombres P",nombre, guitarra);
+        }
+
+        public new void toca(){
+            Console.WriteLine("Pam Pam tananananana pam pam");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Baterista a= new Baterista ("Jojo", "10 piezas");
+            List<Musicos> musico = new List<Musicos>();
+            musico.Add(new Musicos("Joserroni"));
+            musico.Add(new Baterista("Elise","12 piezas"));
+            musico.Add(new Bajista("Slash","Gibson"));
+            musico.Add(new Guitarrista("Jimmy","Fender"));
+
+            foreach(Musicos m in musico){
+                m.afina();
+                m.saluda();
+                m.toca();
+            }
+
+            a.afina();
+            a.saluda();
+            a.toca();
+
+            
+        
+        }
+    }
+}
