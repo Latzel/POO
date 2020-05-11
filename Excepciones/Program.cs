e791b11502878f8e920077a38274363b77ce9aeb
Axel López <axel.dlrlopez@gmail.com>
2020-05-11 12:56:09 -0700

    Excepxiones

---
+++ Excepciones/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Excepciones //capturaedad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Captura tu edad:");
+
+            int edad = -1;
+            bool conexion=true;
+            try{
+                edad = Int16.Parse(Console.ReadLine());
+                edad = edad/0;
+                conexion= false;
+                
+            }
+            catch (FormatException fe ){
+                Console.WriteLine("Digite unicamente numeros");
+                Console.WriteLine(fe.Message);
+
+            }
+
+            catch(OverflowException ){
+                Console.WriteLine("Hey! Una edad a 1000 por favor");
+
+            }
+
+            catch (Exception e){
+                 Console.WriteLine("Error {0}", e.GetType());
+            }
+
+            finally{
+                conexion = false;
+            }
+
+            if(edad !=-1){
+            
+            if (edad > 40){
+                Console.WriteLine("Estas en riesgo");
+            }
+            else{
+                Console.WriteLine("Riesgo bajo");
+            }
+
+            }
+
+            Console.WriteLine(conexion);
+        }
+    }
+}
