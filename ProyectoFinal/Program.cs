67f43606505f26237a05f746f0a1a5dd09d31059
Axel López <axel.dlrlopez@gmail.com>
2020-06-03 23:31:07 -0700

    PF

--- ProyectoFinal/Program.cs
+++ ProyectoFinal/Program.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ProyectoFinal
 {
     class Product
     {
-        public Product(string c, string d, int l){
+        public Product(string c, string d, int dep, int l, Double p){ //Acuerdate de quitar la p
             Codigo=c;
             Descripcion=d;
-            //Precio=p;
+            Precio=p; //PRECIO ALTERNATIVO AXEL
+            Departamento = dep;
             Likes=l;
+            
         }
+        //lista de precios en otro archivo y que se lea desde aqui?
 
         public Product()
         {
@@ -21,13 +25,16 @@ namespace ProyectoFinal
 
         public string Descripcion {get; set;}
 
-        //public Double Precio {get; set;}
+        public Double Precio {get; set;}
 
         public int Likes {get; set;}
 
+        public int Departamento {get; set;}
+
         public override string ToString(){
-            return String.Format("Codigo: {0} Descripcion: {1}  Likes: {2}", Codigo, Descripcion/*, Precio*/, Likes);
+            return String.Format("Codigo: {0} Descripcion: {1}  Dpto: {2} Likes: {3} Precio: {4}", Codigo, Descripcion, Departamento, Likes, Precio); //Precio aqui
         }
+
 
     }
 
@@ -40,7 +47,7 @@ namespace ProyectoFinal
             
             foreach(Product p in products)
             {
-                txtOut.WriteLine("{0}|{1}|{2}", p.Codigo, p.Descripcion/*, p.Precio*/, p.Likes);
+                txtOut.WriteLine("{0}|{1}|{2}|{3}|{4}", p.Codigo, p.Descripcion, p.Departamento, p.Likes, p.Precio);//Precio aqui
             }
 
             txtOut.Close();
@@ -57,17 +64,29 @@ namespace ProyectoFinal
             {
                 string line = txtIn.ReadLine();
                 string[] columns = line.Split('|');
-                Product p = new Product(columns[0],columns[1]/*, Double.Parse(columns[2])*/, int.Parse(columns[3]));
+                Product p = new Product(columns[0],columns[1], int.Parse(columns[2]), int.Parse(columns[3]), Double.Parse(columns[4])); //Pecio aqui
                 products.Add(p);
             }
 
             return products;
-        }
 
+            
+        /* static void GetDepartamento (int depa) //VARIABLE ERROR
+        {
+            IEnumerable<Product> Producto = from p in products
+            where p.Departamento == depa
+            select p;
+
+            Console.WriteLine("El producto pertenece al departamento: "+depa);
+        }
+        */
+            
+        }
+        
     
     }
 
-    class PrecioFecha
+    class PrecioFecha //El producto entra con un precio y por dia disminuye 10 pesos hasta bajar 100?
     {
         DateTime FechaInicio{get; set;}
         DateTime FechaFinal{get; set;}
@@ -81,11 +100,11 @@ namespace ProyectoFinal
         {
             List<Product> products = new List<Product>();
             
-            products.Add(new Product("AAA","Telefono OnePlus 5t", 155));
-            products.Add(new Product("BBB","Telefono OnePlus 6t", 154));
-            products.Add(new Product("CCC","Telefono OnePlus 7t", 153));
-            products.Add(new Product("DDD","Telefono OnePlus 8t", 152));
-            products.Add(new Product("EEE","Telefono OnePlus 9t", 151));
+            products.Add(new Product("AAA","Telefono OnePlus 5t", 2, 155, 1900.99));
+            products.Add(new Product("BBB","Telefono OnePlus 6t", 1, 154, 4500.99));
+            products.Add(new Product("CCC","Telefono OnePlus 7t", 5, 153, 2300.99));
+            products.Add(new Product("DDD","Telefono OnePlus 8t", 4, 152, 2900.99));
+            products.Add(new Product("EEE","Telefono OnePlus 9t", 3, 151, 2000.99));
             
             ProductDB.WriteToTXT(@"C:\Users\axeld\Desktop\productos.txt", products);
     
@@ -93,6 +112,9 @@ namespace ProyectoFinal
 
             foreach(Product p in products)
                 Console.WriteLine(p);
+            
+            
+            
         }
     }
 }
