8c22ea9d76737f02ffe4b3c8dd740ddc8cfbeb14
Axel López <axel.dlrlopez@gmail.com>
2020-06-05 21:45:11 -0700

    PF

--- ProyectoFinal/Program.cs
+++ ProyectoFinal/Program.cs
@@ -15,11 +15,9 @@ namespace ProyectoFinal
             Likes=l;
             
         }
-        //lista de precios en otro archivo y que se lea desde aqui?
 
         public Product()
         {
-
         }
         public string Codigo {get; set;}
 
@@ -37,6 +35,33 @@ namespace ProyectoFinal
 
         public List<PrecioFecha> Precios = new List<PrecioFecha>();
 
+        public static List<Product> GetDepartamento(int depa, List<Product> products)
+        {
+            List<Product> RetDepa = new List<Product>();
+            foreach(Product p in products)
+            {
+                if(p.Departamento == depa)
+                RetDepa.Add(p);
+            }
+            return RetDepa;
+        }
+
+        public static List<Product> GetPrecio(string prec, List<Product> products) 
+        {
+            List<Product> RetPrec = new List<Product>();
+            foreach(Product p in products)
+            {
+                if(p.Codigo == prec)
+                RetPrec.Add(p);
+            }
+            return RetPrec;
+        }  
+/*
+        public static List<Product> AcomodaLikes(List<Product> products) //AQUI VOY
+        {
+            List
+        } 
+*/
 
     }
 
@@ -55,8 +80,7 @@ namespace ProyectoFinal
             txtOut.Close();
 
         }
-
-    
+  
 
         public static List<Product> ReadFromTXT(string path)
         {
@@ -73,45 +97,14 @@ namespace ProyectoFinal
 
             return products;
         }
-        
-        public static List<Product> ReadFromTXT111(string path)
-        {
-            List<Product> products = new List<Product>();
-            StreamReader txtIn = new StreamReader(new FileStream(path, FileMode.Open, FileAccess.Read));
-
-            while(txtIn.Peek()!=-1)
-            {
-                string line = txtIn.ReadLine();
-                string[] columns = line.Split('|');
-                Product p = new Product(columns[0],columns[1], int.Parse(columns[2]), int.Parse(columns[3]), Double.Parse(columns[4])); //Pecio aqui
-                products.Add(p);
-            }
-
-            return products;
-        }
-
-
-/*
-        public static List<Product> GetDepartamento(string depa) //VARIABLE ERROR
-        {
-           IEnumerable<Product> Producto = from p in products
-           where p.Departamento == depa
-           select p;
-            Console.WriteLine("El producto pertenece al departamento: "+depa);
-            
-        }    
-*/
-    
-    
     }
 
-    class PrecioFecha //El producto entra con un precio y por dia disminuye 10 pesos hasta bajar 100?
+    class PrecioFecha
     {
         DateTime FechaInicio{get; set;}
         DateTime FechaFinal{get; set;}
         Decimal Precio{get; set;}
     }
-
 
     class Program
     {
@@ -121,9 +114,9 @@ namespace ProyectoFinal
             
             products.Add(new Product("AAA","Telefono OnePlus 5t", 2, 155, 1900.99));
             products.Add(new Product("BBB","Telefono OnePlus 6t", 1, 154, 4500.99));
-            products.Add(new Product("CCC","Telefono OnePlus 7t", 5, 153, 2300.99));
-            products.Add(new Product("DDD","Telefono OnePlus 8t", 2, 152, 2900.99));
-            products.Add(new Product("EEE","Telefono OnePlus 9t", 3, 151, 2000.99));
+            products.Add(new Product("CCC","Telefono OnePlus 7t", 2, 153, 2300.99));
+            products.Add(new Product("DDD","Telefono OnePlus 8t", 3, 152, 2900.99));
+            products.Add(new Product("EEE","Telefono OnePlus 9t", 1, 151, 2000.99));
             
             ProductDB.WriteToTXT(@"C:\Users\axeld\Desktop\productos.txt", products);
     
@@ -135,19 +128,44 @@ namespace ProyectoFinal
             //ProductDB.GetDepartamento(@"C:\Users\axeld\Desktop\productos.txt", products);
 
             products = ProductDB.ReadFromTXT(@"C:\Users\axeld\Desktop\productos.txt");
-            products = ProductDB.ReadFromTXT111(@"C:\Users\axeld\Desktop\productos.txt");
 
-            //Pedir valor que se va a usar para el dpto
-            Console.WriteLine("Que departamento deseas buscar?");
-            int valor = 0;
-            valor = Int16.Parse(Console.ReadLine());
+            Console.WriteLine("Que accion deseas realizar? \n 1) Buscar por departamento \n 2) Buscar por medio de codigo");
+            try{
+                int caseSwitch = Int16.Parse(Console.ReadLine());
+        
+            switch(caseSwitch)
+            {
+                case 1: //Caso donde se obtienen los productos de cada departamento
+                //try{
+            Console.WriteLine("Que departamento deseas buscar? \n 1)Telefonos \n 2)Computadoras \n 3)Televisores");
+            List<Product> val = Product.GetDepartamento(Int16.Parse(Console.ReadLine()), products);
+            Console.WriteLine("Los productos dentro de ese departamento departamento son: ");
+            foreach(Product p in val)  
+            Console.WriteLine(p);
+                //}catch () //Numero diferente a 1,2,3
+            break;
 
-            Console.WriteLine("Los productos dentro del departamento {} son:",valor);
+                case 2: //Caso donde se obtien el precio de cada prouducto
+                //try{
+            Console.WriteLine("Por favor añada el codigo del producto el cual desea conocer su precio");
+            List<Product> cod = Product.GetPrecio(Console.ReadLine(), products);
+            Console.WriteLine("El precio de su producto es de: ");
+            foreach(Product p in cod)
+            Console.WriteLine("El precio del producto {0}-{1} es de: {2}$",p.Codigo, p.Descripcion, p.Precio);
+                //}catch () //Codigo de 3 letras "XXX"
+            break;     
 
-            //foreach(Product p in products)
-            //Console.WriteLine(p.Departamento);
+                case 3:
+                 Product.AcomodaLikes();
 
+            }
+
+             }catch (FormatException fe ){
+                Console.WriteLine("Digite unicamente numeros");
+                Console.WriteLine(fe.Message);
+            }
 
         }
     }
+
 }
